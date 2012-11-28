3f9475ce9629472744280203620d68f4d70367ea
Jack Lu <jacklu@clanent.net>
2012-11-28 13:39:09 -0500

    eh..

--- include/database.php
+++ include/database.php
@@ -57,6 +57,19 @@ function databaseSetup($service_id) {
 	}
 }
 
+//returns array of realm_server
+// this is an internal function, so the database link must be provided
+function databaseGetRealms($link) {
+	$result = mysql_query("SELECT DISTINCT spoofedrealm FROM gameplayers", $link);
+	$array = array();
+	
+	while($row = mysql_fetch_array($result)) {
+		$array[] = $row[0];
+	}
+	
+	return $array;
+}
+
 //gets current games based on gamelist patch
 // gamelist patch is available at http://www.codelain.com/forum/index.php?topic=18076.0
 //returns array of ('botid' =. botid, 'gamename' => gamename, 'ownername' => ownername, 'creatorname' => creatorname, 'map' => map, 'slotstaken' => slotstaken, 'slotstotal' => slotstotal, 'usernames' => usernames, 'totalgames' => totalgames, 'totalplayers' => totalplayers)
@@ -141,7 +154,18 @@ function databaseBanUser($service_id, $username, $realm, $duration, $reason, $un
 	$duration = escape(intval($duration) * 3600);
 	$reason = escape($reason);
 		
-	$realms = array("uswest.battle.net", "useast.battle.net", "europe.battle.net", "asia.battle.net", "");
+	$realms = databaseGetRealms($link);
+	
+	//find a realm to use by default that is not blank
+	$default_realm = "";
+	
+	for($realms as $realm) {
+		if($realm != "") {
+			$default_realm = $realm;
+			break;
+		}
+	}
+	
 	if($realm != "") $realms = array($realm);
 		
 	foreach($realms as $realm_it) {
@@ -167,9 +191,9 @@ function databaseBanUser($service_id, $username, $realm, $duration, $reason, $un
 			while($row = mysql_fetch_row($result)) {
 				$ip = escape($row[0]);
 				
-				//if this is for non-spoofchecked users, ban on USWest by default
+				//if this is for non-spoofchecked users, ban on default realm
 				$ban_realm = $realm_it;
-				if($ban_realm == "") $ban_realm = "uswest.battle.net";
+				if($ban_realm == "") $ban_realm = $default_realm;
 				
 				mysql_query("INSERT INTO bans (botid, server, name, ip, date, gamename, admin, reason, expiredate) VALUES ('0', '$ban_realm', '$username', '$ip', CURDATE(), '', '$username_clean', '$reason', DATE_ADD( NOW( ), INTERVAL $duration second ))", $link);
 			}
@@ -179,6 +203,120 @@ function databaseBanUser($service_id, $username, $realm, $duration, $reason, $un
 				mysql_query("INSERT INTO bans (botid, server, name, ip, date, gamename, admin, reason, expiredate) VALUES ('0', '$realm_it', '$username', '', CURDATE(), '', '$username_clean', '$reason', DATE_ADD( NOW( ), INTERVAL $duration second ))", $link);
 			}
 		}
+	}
+}
+
+//deletes a ban
+function databaseDeleteBan($service_id, $ban_id) {
+	$link = databaseConnect($service_id);
+	
+	if(!$link) {
+		return;
+	}
+	
+	$ban_id = escape($ban_id);
+	mysql_query("DELETE FROM bans WHERE id = '$ban_id'", $link);
+}
+
+//returns array of id => (name, server, ip, admin, gamename, reason, date, expiredate)
+function databaseGetBans($service_id) {
+	$link = databaseConnect($service_id);
+	
+	if(!$link) {
+		return array();
+	}
+	
+	$result = mysql_query("SELECT id, name, server, ip, admin, gamename, reason, date, expiredate FROM bans ORDER BY id", $link);
+	$array = array();
+	
+	while($row = mysql_fetch_array($result)) {
+		$array[$row[0]] = array('name' => $row[1], 'server' => $row[2], 'ip' => $row[3], 'admin' => $row[4], 'gamename' => $row[5], 'reason' => $row[6], 'date' => $row[7], 'expiredate'] => $row[8]);
+	}
+	
+	return $array;
+}
+
+function searchUser($service_id, $username, $realm) {
+	$link = databaseConnect($service_id);
+	
+	if(!$link) {
+		return array();
+	}
+	
+	$username = escape($username);
+	$realm = escape($realm);
+	
+	$realms = databaseGetRealms($link);
+	if($realm != "") $realms = array($realm);
+	
+	foreach($realms as $realm_it) {
+		$where = "WHERE name = '$username'";
+		if($realm_it != "*") $where .= " AND realm = '$realm_it'";
+		
+		//grab general statistics
+		$result = mysql_query("SELECT time_created, time_active, num_games, (total_leftpercent / num_games)*100, lastgames FROM gametrack $where", $link);
+		$row = mysql_fetch_row($result);
+		
+		$firstgame = uxtDate(convertTime($row[0]));
+		$lastgame = uxtDate(convertTime($row[1]));
+		$totalgames = $row[2];
+		$leftpercent = $row[3];
+		$lastgames = $row[4];
+		
+		echo "<h2>$username@$realm_it</h2>";
+		
+		if($totalgames != 0) {
+			echo "<h3>General statistics</h3>";
+			echo "<table>";
+			echo "<tr><th>Field</th><th>Value</th></tr>";
+			echo "<tr><td>First game</td><td>" . $firstgame . "</td></tr>";
+			echo "<tr><td>Last game</td><td>" . $lastgame . "</td></tr>";
+			echo "<tr><td>Total games</td><td>" . $totalgames . "</td></tr>";
+			echo "<tr><td>Left percent</td><td>" . $leftpercent . "</td></tr>";
+			echo "</table>";
+		
+			//last few games
+			echo "<h3>Last few games</h3>";
+			
+			$lastgames = explode(",", $lastgames);
+			
+			echo "<ul>";
+			//process in reverse
+			for($i = count($lastgames) - 1; $i >= 0; $i--) {
+				$gameid = $lastgames[$i];
+				$result = mysql_query("SELECT gamename FROM games WHERE id = '$gameid'");
+				
+				if($row = mysql_fetch_array($result)) {
+					echo "<li><a href=\"game.php?id=$gameid\">" . $row[0] . "</a></li>";
+				}
+			}
+			echo "<li><b><a href=\"games.php?username=$username&realm=$realm_it\">More</a></b></li>";
+			echo "</ul>";
+			
+			//ban history
+			$result = mysql_query("SELECT admin, reason, gamename, date, expiredate FROM ban_history WHERE name = '$username' AND server = '$realm_it' ORDER BY id DESC LIMIT 6");
+			
+			if(mysql_num_rows($result) > 0) {
+				echo "<h3>Ban history</h3>";
+			
+				echo "<table cellpadding=\"2\">";
+				echo "<tr><th>Admin</th><th>Reason</th><th>Gamename</th><th>Date</th><th>Expire</th></tr>";
+				
+				while($row = mysql_fetch_row($result)) {
+					echo "<tr>";
+					echo "<td>" . $row[0] . "</td>";
+					echo "<td>" . $row[1] . "</td>";
+					echo "<td>" . $row[2] . "</td>";
+					echo "<td>" . $row[3] . "</td>";
+					echo "<td>" . uxtDate(convertTime($row[4])) . "</td>";
+					echo "</tr>";
+				}
+				echo "</table>";
+			}
+		} else {
+			echo "<p><b><i>No games found for this user.</i></b></p>";
+		}
+	}
 }
 
 ?>
