5ff10e755e465e880fb5dc9ca416aa7be253c2af
Jack Lu <jacklu@clanent.net>
2012-12-01 22:16:38 -0500

    Log pages now autoscroll to the bottom.

--- style/channel/log.php
+++ style/channel/log.php
@@ -3,7 +3,12 @@
 <p>Your log file appears below.</p>
 
 <? if($log !== false) { ?>
-	<textarea rows="30" name="content" class="field span12" readonly="true"><? foreach($log as $line) { echo htmlspecialchars($line) . "\n"; } ?></textarea>
+	<textarea rows="30" id="log" name="content" class="field span12" readonly="true"><? foreach($log as $line) { echo htmlspecialchars($line) . "\n"; } ?></textarea>
+	
+	<script type="text/javascript">
+	var textarea = document.getElementById('log');
+	textarea.scrollTop = textarea.scrollHeight;
+	</script>
 <? } else { ?>
 	<p><b><i>Error while reading log: probably doesn't exist.</i></b></p>
 <? } ?>
