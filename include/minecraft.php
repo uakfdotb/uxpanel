341d1b5452bd34b2265e648b220925a8373213de
uakf.b <perennate@gmail.com>
2013-03-29 11:00:03 -0500

    Copy master.

--- include/minecraft.php
+++ include/minecraft.php
@@ -523,7 +523,7 @@ function minecraftServerUpload($service_id, $files, $type = "plugin") {
 	//check if we're allowed to upload files
 	$uploadable = getServiceParam("service_id", "uploadable");
 	
-	if($uploadable != 1) {
+	if($uploadable === false) {
 		return "Uploads are not allowed for this service; instead, try using the linking form.";
 	}
 	
